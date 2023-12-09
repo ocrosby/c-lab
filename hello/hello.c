f91af116301ea1560c837858bf7088945a8dc834
Omar Crosby <omar.crosby@gmail.com>
2023-12-09 13:31:19 -0500

    chore: update hello.c

--- hello/hello.c
+++ hello/hello.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    int x;
-
-    x = 44;
-
-    printf("%d\n", x);
-    printf("Hello Jordyn!");
+    printf("hello, world\n");
 
     return(0);
 }
